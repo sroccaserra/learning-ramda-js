d594c25e003a9935a802bf5a7e60ad3da9a07544
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-03-17 16:58:15 +0100

    Move "double" function up

--- test.js
+++ test.js
@@ -2,28 +2,22 @@ const { expect } = require('chai');
 const R = require('ramda');
 const { Maybe, Either } = require('ramda-fantasy');
 
-describe('map', () => {
-  it('applies a function to an array', () => {
-    // double :: Number -> Number
-    const double = x => x * 2;
+describe('R.map', () => {
+  // double :: Number -> Number
+  const double = x => x * 2;
 
+  it('applies a function to an array', () => {
     const result = R.map(double, [1, 2, 3]);
 
     expect(result).to.deep.equal([2, 4, 6]);
   });
 
   it('applies a function to an object', () => {
-    // double :: Number -> Number
-    const double = x => x * 2;
-
     const result = R.map(double, {x: 1, y: 2, z: 3});
 
     expect(result).to.deep.equal({x: 2, y: 4, z: 6});
   });
 });
-
-// const lookup = R.curry((k, obj) => k in obj ? Just(obj[k]) : Nothing());
-
 
 describe('safeDiv and safeInvert examples', () => {
   // safeDiv :: Number -> Number -> Maybe Number
